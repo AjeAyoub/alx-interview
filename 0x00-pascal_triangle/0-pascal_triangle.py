1dfd169c23437107e2fcfa59c8663f7cfca30709
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-07-12 19:46:53 +0100

    Starwars_api

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,41 @@
-#!/usr/bin/python3
-"""
-0-pascal_triangle
-"""
+#!/usr/bin/node
+/**
+ * Wrapper function for request object that allows it
+ * to work with async and await
+ * @param   {String} url - site url
+ * @returns {Promise}    - promise object that resolves
+ *                         with parsed JSON response
+ *                         and rejects with the request error.
+ */
+function makeRequest (url) {
+  const request = require('request');
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
+    });
+  });
+}
 
+/**
+ * Entry point - makes requests to Star Wars API
+ * for movie info based movie ID passed as a CLI parameter.
+ * Retrieves movie character info then prints their names
+ * in order of appearance in the initial response.
+ */
+async function main () {
+  const args = process.argv;
 
-def pascal_triangle(n):
-    """
-    Returns list of integers
-    representing the Pascal Triangle of n
-    returns empty list if n <= 0
-    """
-    q = []
-    if n <= 0:
-        return q
-    q = [[1]]
-    for i in range(1, n):
-        temp = [1]
-        for j in range(len(q[i - 1]) - 1):
-            curr = q[i - 1]
-            temp.append(q[i - 1][j] + q[i - 1][j + 1])
-        temp.append(1)
-        q.append(temp)
-    return q
+  if (args.length < 3) return;
+
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+  const movie = await makeRequest(movieUrl);
+
+  if (movie.characters === undefined) return;
+  for (const characterUrl of movie.characters) {
+    const character = await makeRequest(characterUrl);
+    console.log(character.name);
+  }
+}
+
+main();
